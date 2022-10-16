ff35bf0f842082d501b5f0a121616ae8efd2def9
Bobipawa <walterpatrick377@gmail.com>
2022-10-16 04:37:55 -0700

    ah

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return: 0 always
+ */
+int main(void)
+{
+int i = 0, j = 0;
+for (i = 48; i <= 56; i++)
+{
+for (j = i + 1; j <= 57; j++)
+{
+putchar(i);
+putchar(j);
+if (i == 56 && j == 57)
+{
+break;
+}
+putchar(',');
+putchar(' ');
+}
+}
+return (0);
+}
