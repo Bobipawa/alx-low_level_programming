b31890e48f5ff09855157d4b4f5c7487c4750bc3
Bobipawa <walterpatrick377@gmail.com>
2022-10-18 06:32:22 -0700

    ah

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+void print_alphabet(void);
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+print_alphabet();
+return (0);
+}
+void print_alphabet(void)
+{
+int i;
+for (i = 0; i <= 25; i++)
+{
+putchar('0'+i);
+}
+putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+void print_alphabet_x10(void);
+/**
+ *main - Entry point
+ *Return: 0 always
+ */
+int main()
+{
+print_alphabet_x10();
+return (0);
+}
+void print_alphabet_x10(void)
+{
+int cpt = 0, i = 0;
+while (cpt < 10)
+{
+cpt++;
+for (i = 0; i <= 25; i++)
+{
+putchar('a'+i);
+}
+putchar('\n');
+}
+}
