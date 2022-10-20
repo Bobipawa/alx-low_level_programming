d03f44871538fa71e91177caaafa6c4f04c959e5
Bobipawa <walterpatrick377@gmail.com>
2022-10-20 14:28:59 -0700

    ah

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *_isupper - CHecks if th parameter c is an uppercase letter
+ * @c: Int representing a character
+ * Return: Always 0.
+ */
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+return (1);
+return (0);
+}
