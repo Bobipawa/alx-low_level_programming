0459910caa0767798116f9cbafd6f1bf1dd63bfc
Bobipawa <walterpatrick377@gmail.com>
2022-10-24 10:30:20 -0700

    ah

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,10 +6,17 @@
  */
 int _atoi(char *s)
 {
-int i, nb = 0;
+  int i, nb = 0, signe = 1;
 for (i = 0; s[i] != '\0'; i++)
 {
+if (s[i] >= 48 && s[i] <= 57)
 nb = nb * 10 + s[i] - '0';
+else if (s[i] == '-')
+signe = -1;
+else if (s[i] == '+')
+continue;
+else
+break;
 }
 return (nb);
 }
