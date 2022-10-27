feed2b8a9b6cea1541ba7f30a151b32926680927
Bobipawa <walterpatrick377@gmail.com>
2022-10-27 04:15:54 -0700

    ah

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *reverse_array - Reverses an array
+ *@a: an array
+ *@n: length of the array
+ */
+void reverse_array(int *a, int n)
+{
+int i = 0, j = n - 1, pivot;
+while (i < k)
+{
+pivot = a[i];
+a[i] = a[j];
+a[j] = pivot;
+i++;
+j--;
+}
+}
