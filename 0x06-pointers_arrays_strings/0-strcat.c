4f0607698bb70e8842dac9345fcae36900ceed02
Bobipawa <walterpatrick377@gmail.com>
2022-10-27 00:52:26 -0700

    ah

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+/**
+ * *_strcat - Concatenates two strings
+ *@dest: a string
+ *@src: string
+ *Return: A pointer on desr
+ */
+char *_strcat(char *dest, char *src)
+{
+int i = 0;
+while (dest[i] != '\0')
+{
+i++;
+}
+int k = 0;
+while (src[k] != '\0')
+{
+dest[i + k] = src[k];
+k++;
+}
+dest[i + k] = '\0';
+return (dest);
+}
